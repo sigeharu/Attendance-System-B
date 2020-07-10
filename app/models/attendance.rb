7d272479798a2082b75febb7cfc7927ad2a77431
sigeharu <sigeharumiyamoto@gmail.com>
2020-07-10 02:13:23 +0000

    出退勤時間15minutes設定完了

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,7 +6,7 @@ class Attendance < ApplicationRecord
   
   # 出勤時間が存在しない場合に、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
-  # 出勤・退勤時間ごちらも存在する時、出勤時間より早い退勤時間は無効
+  # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
   
   def finished_at_is_invalid_without_a_started_at
